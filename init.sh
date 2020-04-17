4f2a5be709bdb77460bbba10dce960d96e050cf3
ilya radovilsky <blockinhead@gmail.com>
2020-04-17 22:53:56 +0300

    conf fixes

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/conf.conf
 sudo /etc/init.d/nginx restart
