fd8938f2d028561ea23aeaad1b4e646302db15f3
ilya radovilsky <blockinhead@gmail.com>
2020-04-19 22:18:36 +0300

    added wsgi

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+# hello.py is a module, app is a function in it
+gunicorn -b 0.0.0.0:8080 hello:app &
+
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/conf.conf
 sudo /etc/init.d/nginx restart
