373e3a58cab770a98dc9fc977858888c76000881
ilya radovilsky <blockinhead@gmail.com>
2020-04-22 21:28:07 +0300

    updated app and routing

---
+++ ask/qa/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.contrib import admin
+from views import test
+
+urlpatterns = [
+    # url(r'^admin/', admin.site.urls),
+    url(r'^(\d+)/$', test),
+]
--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 # hello.py is a module, app is a function in it
 gunicorn -b 0.0.0.0:8080 hello:app &
+cd ./ask/
+gunicorn -b 0.0.0.0:8000 ask.wsgi &
 
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/conf.conf
