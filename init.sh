16992ff1f39b52e0f654b83e5124b15fdd63c65e
ir <blockinhead@gmail.com>
2020-04-26 15:55:25 +0300

    added mysql initialization and model

--- init.sh
+++ init.sh
@@ -1,6 +1,15 @@
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database stepic_webtech;"
+mysql -uroot -e "grant all privileges on stepic_webtech.* to 'box'@'localhost' with grant option;"
+
+cd ~/web/
+
 # hello.py is a module, app is a function in it
 gunicorn -b 0.0.0.0:8080 hello:app &
-cd ./ask/
+
+cd ~web/ask/
+./manage.py makemigrations
+./manage.py migrate
 gunicorn -b 0.0.0.0:8000 ask.wsgi &
 
 sudo unlink /etc/nginx/sites-enabled/default
